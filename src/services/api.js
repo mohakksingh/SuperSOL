7a0c7c0175824910983281879e3a2ba40cb4b747
mohakksingh <singhmohak999@gmail.com>
2024-09-02 21:41:54 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
-
+// const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://supersol-backend.onrender.com/api';
 export const register = (username, password, walletAddress) => 
   axios.post(`${API_URL}/auth/register`, { username, password, walletAddress });
 
