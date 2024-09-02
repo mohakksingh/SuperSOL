e9510f038c199bf1c9477b30e46e7fcf147472f5
mohakksingh <singhmohak999@gmail.com>
2024-09-02 21:32:28 +0530

    pusheddd

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,31 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      crypto: 'crypto-browserify',
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis',
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
     },
   },
 })
+
